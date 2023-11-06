6051e78faf894c99f922a9d3153200693aaea2a7
KerimovaLeyla <sd79pqh0s@gmail.com>
2023-11-06 13:40:21 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// $("#btn1").click(function(){
+//     $(".div").hide();
+
+//   });
+  
+//   $("#btn2").click(function(){
+   
+//     $(".div").show();
+//   });
+  
+//   $("#btn3").click(function(){
+//     $(".div").toggle();
+ 
+//   });
+
+
+
+
+
+// $(document).ready(function(){
+//     $("#btn1").click(function(){
+      
+//         $(".div1").slideToggle();
+        
+//       });
+//       $("#btn2").click(function(){
+//         $(".div2").slideToggle();
+        
+        
+//       });
+//       $("#btn3").click(function(){
+//         $(".div3").slideToggle();
+        
+        
+//       });
+//   });
+$("h1").mouseover(function(){
+    $("h1").css("background-color", "red");
+    $("h1").css("color", "black");
+  });
+  $("h1").mouseleave(function(){
+    $("h1").css("background-color", "orange");
+    $("h1").css("color", "white");
+  });
+
+
